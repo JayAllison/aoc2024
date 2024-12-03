cf597ef1ca8e05e8ad7022a56b5d6bad1440e281
Jay Allison <jay.allison@acuitybrands.com>
2024-12-03 08:58:21 -0600

    I forgot the () on the operators - does not affect the outcome, but I wanted to correct that

--- day03/puzzle2.py
+++ day03/puzzle2.py
@@ -3,15 +3,15 @@ import re
 # filename = "sample2.txt"
 filename = "input.txt"
 
-parser: re.Pattern = re.compile(r'(don\'t)|(do)|mul\((\d+),(\d+)\)')
+parser: re.Pattern = re.compile(r'(don\'t\(\))|(do\(\))|mul\((\d+),(\d+)\)')
 operators_and_pairs_of_numbers: list[tuple[str]] = parser.findall(open(filename).read())
 product_sum = 0
 enabled = True
 while operators_and_pairs_of_numbers:
     match = operators_and_pairs_of_numbers.pop(0)
-    if match[0] == "don't":
+    if match[0] == "don't()":
         enabled = False
-    elif match[1] == "do":
+    elif match[1] == "do()":
         enabled = True
     else:
         if enabled:
