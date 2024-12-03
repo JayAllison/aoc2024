2abf3b40156606339cbcccfc354790225a759fc8
Jay Allison <jay.allison@acuitybrands.com>
2024-12-03 08:37:01 -0600

    Using reduce() for Day 3, Part 1

--- day03/puzzle1.py
+++ day03/puzzle1.py
@@ -1,3 +1,4 @@
+from functools import reduce
 import re
 
 # filename = "sample.txt"
@@ -5,8 +6,4 @@ filename = "input.txt"
 
 parser: re.Pattern = re.compile(r'mul\((\d+),(\d+)\)')
 pairs_of_numbers: list[tuple[str]] = parser.findall(open(filename).read())
-product_sum = 0
-while pairs_of_numbers:
-    pair = pairs_of_numbers.pop()
-    product_sum += int(pair[0]) * int(pair[1])
-print(product_sum)
+print(reduce(lambda a, t: a + int(t[0]) * int(t[1]), pairs_of_numbers, 0))
