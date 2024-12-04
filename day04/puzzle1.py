1469cadf7ad6ff9972eb6ffa75cdddd60f81cc3b
Jay Allison <jay.allison@acuitybrands.com>
2024-12-04 12:37:03 -0600

    Tweaks

--- day04/puzzle1.py
+++ day04/puzzle1.py
@@ -3,32 +3,24 @@ import itertools
 # filename = "sample.txt"
 filename = "input.txt"
 
-lines = [line.rstrip() for line in open(filename).readlines()]
+board = [line.rstrip() for line in open(filename).readlines()]
+y_max = len(board)
+x_max = len(board[0])
 
-starts = []
-y_max = len(lines)
-x_max = len(lines[0])
+starts = [(x, y) for x, y in itertools.product(range(x_max), range(y_max)) if board[y][x] == 'X']
 
-for y in range(y_max):
-    for x in range(x_max):
-        if lines[y][x] == 'X':
-            starts.append((x, y))
+directions = [(x, y) for x in (-1, 0, +1) for y in (-1, 0, +1) if x != 0 or y != 0]
 
-directions = list(itertools.product([-1, 0, 1], [-1, 0, 1]))
-directions.remove((0, 0))
-
+word = 'MAS'
 count = 0
 for x_s, y_s in starts:
     for dx, dy in directions:
-        x = x_s + dx
-        y = y_s + dy
-        if 0 <= x < x_max and 0 <= y < y_max and lines[y][x] == 'M':
-            x += dx
-            y += dy
-            if 0 <= x < x_max and 0 <= y < y_max and lines[y][x] == 'A':
-                x += dx
-                y += dy
-                if 0 <= x < x_max and 0 <= y < y_max and lines[y][x] == 'S':
+        x, y = (x_s + dx, y_s + dy)
+        if 0 <= x < x_max and 0 <= y < y_max and board[y][x] == 'M':
+            x, y = (x + dx, y + dy)
+            if 0 <= x < x_max and 0 <= y < y_max and board[y][x] == 'A':
+                x, y = (x + dx, y + dy)
+                if 0 <= x < x_max and 0 <= y < y_max and board[y][x] == 'S':
                     count += 1
 
 print(count)
--- day04/puzzle2.py
+++ day04/puzzle2.py
@@ -1,37 +1,30 @@
+import itertools
+
 # filename = "sample.txt"
 filename = "input.txt"
 
-lines = [line.rstrip() for line in open(filename).readlines()]
-
-starts = []
-y_max = len(lines)
-x_max = len(lines[0])
+board = [line.rstrip() for line in open(filename).readlines()]
+y_max = len(board)
+x_max = len(board[0])
 
 
-def check_mas(a: tuple, m: list[tuple], s: list[tuple]) -> bool:
-    x_m1 = a[0] + m[0][0]
-    y_m1 = a[1] + m[0][1]
-    x_m2 = a[0] + m[1][0]
-    y_m2 = a[1] + m[1][1]
+def check_for_mas(a: tuple, m: list[tuple], s: list[tuple]) -> bool:
+    x_m1, y_m1 = (a[0] + m[0][0], a[1] + m[0][1])
+    x_m2, y_m2 = (a[0] + m[1][0], a[1] + m[1][1])
 
-    x_s1 = a[0] + s[0][0]
-    y_s1 = a[1] + s[0][1]
-    x_s2 = a[0] + s[1][0]
-    y_s2 = a[1] + s[1][1]
+    x_s1, y_s1 = (a[0] + s[0][0], a[1] + s[0][1])
+    x_s2, y_s2 = (a[0] + s[1][0], a[1] + s[1][1])
 
-    if 0 <= x_m1 < x_max and 0 <= y_m1 < y_max and lines[y_m1][x_m1] == 'M':
-        if 0 <= x_m2 < x_max and 0 <= y_m2 < y_max and lines[y_m2][x_m2] == 'M':
-            if 0 <= x_s1 < x_max and 0 <= y_s1 < y_max and lines[y_s1][x_s1] == 'S':
-                if 0 <= x_s2 < x_max and 0 <= y_s2 < y_max and lines[y_s2][x_s2] == 'S':
+    if 0 <= x_m1 < x_max and 0 <= y_m1 < y_max and board[y_m1][x_m1] == 'M':
+        if 0 <= x_m2 < x_max and 0 <= y_m2 < y_max and board[y_m2][x_m2] == 'M':
+            if 0 <= x_s1 < x_max and 0 <= y_s1 < y_max and board[y_s1][x_s1] == 'S':
+                if 0 <= x_s2 < x_max and 0 <= y_s2 < y_max and board[y_s2][x_s2] == 'S':
                     return True
 
     return False
 
 
-for yy in range(y_max):
-    for xx in range(x_max):
-        if lines[yy][xx] == 'A':
-            starts.append((xx, yy))
+starts = [(x, y) for x, y in itertools.product(range(x_max), range(y_max)) if board[y][x] == 'A']
 
 up = [(-1, -1), (+1, -1)]
 dn = [(-1, +1), (+1, +1)]
@@ -39,8 +32,8 @@ lt = [(-1, -1), (-1, +1)]
 rt = [(+1, -1), (+1, +1)]
 
 count = 0
-for point in starts:
-    if check_mas(point, up, dn) or check_mas(point, dn, up) or check_mas(point, lt, rt) or check_mas(point, rt, lt):
+for pt in starts:
+    if check_for_mas(pt, up, dn) or check_for_mas(pt, dn, up) or check_for_mas(pt, lt, rt) or check_for_mas(pt, rt, lt):
         count += 1
 
 print(count)
